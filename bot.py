faa205582c2e9bc0fb3bd232c52df89d06acccfc
mohamadtahajedi-design <mohamadtahajedi@gmail.com>
2026-05-29 15:24:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,9 @@ STORAGE_CHANNEL = -1003815866775
 
 CHANNELS = [
     
+    -1001335479982,
+    -1002244333591
+
     ("𝐅𝐄𝐋𝐎𝐑𝐢𝐀", "https://t.me/+VOJr8t_ERogxMzc0"),
     ("‌𝖬𝟢𝖱𝖻𝖾𝖫 .", "https://t.me/+8xEA_iR3kDoyN2Zk"),
 
